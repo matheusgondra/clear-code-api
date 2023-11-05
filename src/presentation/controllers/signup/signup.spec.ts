356e08935820d1354214012ce6c9987b7669ff28
Matheus de Gondra <matheusgondra951@gmail.com>
2023-11-05 13:14:18 -0300

    refactor: move Validation to protocols folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -2,7 +2,7 @@ import { SignUpController } from './signup'
 import { MissingParamError, ServerError } from '../../errors'
 import { AddAccount, AddAccountModel, AccountModel, HttpRequest } from './signup-protocols'
 import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
-import { Validation } from '../../helpers/validators/validation'
+import { Validation } from '../../protocols/validation'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
